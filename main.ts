7ac0b1ef1c6fa11503ea0abcf79e332c4f9ff8a4
Dat Vi <datvithanhh@gmail.com>
2024-01-01 21:16:24 +0800

    working plugin

--- main.ts
+++ main.ts
@@ -1,24 +1,52 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, getAllTags, Setting, TFile } from 'obsidian';
+import { copyFile } from 'fs';
+import { join } from 'path';
 // Remember to rename these classes and interfaces!
 
 interface MyPluginSettings {
-	mySetting: string;
+	tag: string;
+	noteFolder: string;
+	attachmentFolder: string;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+	tag: '#publish',
+	noteFolder: '/Users/datvithanh/Project/datvt/content',
+	attachmentFolder: '/Users/datvithanh/Project/datvt/content/notes/images'
 }
 
-export default class MyPlugin extends Plugin {
+export default class NotesExtractionPlugin extends Plugin {
 	settings: MyPluginSettings;
 
 	async onload() {
 		await this.loadSettings();
-
+		const basePath = (this.app.vault.adapter as any).basePath;
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
+		const ribbonIconEl = this.addRibbonIcon('crossed-star', 'Publish notes', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
+			const notes: TFile[] = this.app.vault.getMarkdownFiles();
+			for (const noteFile of notes) {
+				const fileCachedData = this.app.metadataCache.getFileCache(noteFile) || {};
+				const tags = getAllTags(fileCachedData) || [];
+				
+				if (tags.some((tag) => tag === '#publish') && !noteFile.path.startsWith("_meta")) {
+					// copy this file to an os path
+					copyFile(join(basePath, noteFile.path), join(this.settings.noteFolder, noteFile.name), (err) => {
+						if (err) throw err;
+						console.log('file copied to destination.txt');
+					});
+					// copy all of the attachment to a different os path
+					const embeds = fileCachedData.embeds ?? [];
+					console.log(basePath);
+					for (const embed of embeds) {
+						copyFile(join(basePath, "_meta/_files", embed.link), join(this.settings.attachmentFolder, embed.link), (err) => {
+							if (err) throw err;
+							console.log('file copied to destination.txt');
+						});
+					}
+				}
+			}
+	
 			new Notice('This is a notice!');
 		});
 		// Perform additional things with the ribbon
@@ -66,7 +94,7 @@ export default class MyPlugin extends Plugin {
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new NotesExtractionPlugginSettingTab(this.app, this));
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
@@ -107,10 +135,10 @@ class SampleModal extends Modal {
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class NotesExtractionPlugginSettingTab extends PluginSettingTab {
+	plugin: NotesExtractionPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: NotesExtractionPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -121,13 +149,27 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
+			.setName('Note extraction setting')
 			.setDesc('It\'s a secret')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('Tag to copy to')
+				.setValue(this.plugin.settings.tag)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.tag = value;
+					await this.plugin.saveSettings();
+				}))
+			.addText(text => text
+				.setPlaceholder('Note folder')
+				.setValue(this.plugin.settings.noteFolder)
+				.onChange(async (value) => {
+					this.plugin.settings.noteFolder = value;
+					await this.plugin.saveSettings();
+				}))
+			.addText(text => text
+				.setPlaceholder('Attachment folder')
+				.setValue(this.plugin.settings.attachmentFolder)
+				.onChange(async (value) => {
+					this.plugin.settings.attachmentFolder = value;
 					await this.plugin.saveSettings();
 				}));
 	}
